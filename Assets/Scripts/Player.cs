6161004b5d1e21a7b4821af97c8e5d099846ccc8
cbb1997 <cbd45068@gmail.com>
2025-01-09 10:41:18 -0800

    Made Game Function

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed = 1.0f;
+    [SerializeField] private Rigidbody2D _body2D;
     [SerializeField] private Transform _playerTransform;
     [SerializeField] private GameObject _plantPrefab;
     [SerializeField] private int _numSeeds = 5; 
@@ -13,16 +14,26 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
     }
 
     private void Update()
     {
-        
+        float xInput = Input.GetAxis("Horizontal"), yInput = Input.GetAxis("Vertical");
+        _body2D.velocity = new Vector2 (xInput * _speed, yInput * _speed);
+
+        if (Input.GetKeyDown(KeyCode.Space)) PlantSeed();
     }
 
     public void PlantSeed ()
     {
-        
+        if (_numSeedsLeft == 0) return;
+
+        _numSeedsLeft--;
+        _numSeedsPlanted++;
+        Instantiate(_plantPrefab, new Vector2 (_playerTransform.position.x, _playerTransform.position.y), Quaternion.identity);
+
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 }
